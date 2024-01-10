b7a00b673c65d316163c58acd6a61af77b7a1508
OctaciusOlimpo <octaciusolimpo@gmail.com>
2024-01-10 11:05:35 -0300

    Funcionamento Master problemático

--- src/master.cpp
+++ src/master.cpp
@@ -1,7 +1,7 @@
 #include "master.h"
 
 //Intervalo entre os envios
-#define INTERVAL 2000
+#define INTERVAL 5000
 #define MAX_DISPLAY_LINES 4
 
 //std::map<String, bool> nodeSentStatus; // Mapa para rastrear o status de envio de cada node
@@ -9,10 +9,9 @@
 //Tempo do último envio
 long lastSendTime = 0;
 int id = 0;
+int oldId;
 
 int idDisplay;
-
-bool flag;
 
 void setupMaster()
 {
@@ -46,9 +45,6 @@ void setupMaster()
 
 void loopMaster()
 {
-
-  flag = false;
-
   //Se passou o tempo definido em INTERVAL desde o último envio
   if (millis() - lastSendTime > INTERVAL)
   {
@@ -58,6 +54,7 @@ void loopMaster()
     send(id);
 
      // Mova para o próximo escravo usando circular
+    
     id = (id + 1) % numNodes;
   }
 
@@ -109,12 +106,9 @@ void receive()
       String readingID = data.substring(0, pos1);
       String temperatureRef = data.substring(pos1 + 1, pos2);
       String humidityRef = data.substring(pos2 + 1, data.length());
-      
-      while(!flag)
-      {
-        sendToAPI(readingID, temperatureRef, humidityRef, id);  
-      }
-      
+
+      sendToAPI(readingID, temperatureRef, humidityRef, id);  
+
       if(idDisplay <= MAX_DISPLAY_LINES)
       {
         display.clear();
@@ -133,8 +127,9 @@ void receive()
       //                 C  L
       display.drawString(0, 50, "Tempo: " + waiting + "ms");
       display.display();
+      
     }
-  }
+  }    
 }
 
 void sendToAPI(String readingID, String temperatureAP, String humidityAP, int id)
@@ -157,15 +152,7 @@ void sendToAPI(String readingID, String temperatureAP, String humidityAP, int id
     postStr += "&field" + String((id+2)*2 - 2) + "=";
     postStr += String(humidityAP);
 
-    flag = true;
-
     postStr += "\r\n\r\n\r\n\r\n";
-       
-    /*    
-      postStr += "&field4=";
-      postStr += String(rssi);
-      postStr += "\r\n\r\n\r\n\r\n";
-    */
 
     client.print("POST /update HTTP/1.1\n");
     client.print("Host: api.thingspeak.com\n");
